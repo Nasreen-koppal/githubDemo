0364fe77ef006dd355354f7e609811558d0a42d5
Nasreen <nasreekoppal@gmail.com>
2022-08-03 20:02:19 +0530

    [Nasreen]Added the employee wage problem 7

---
+++ empWageUC7.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -x
+
+partTime=1;
+fullTime=2;
+maximumHoursPerMonth=4;
+empRatePerHour=20;
+numberofworkingdays=20;
+
+totalempHours=0;
+totalworkingdays=0;
+
+function getworkingHours()
+{
+
+        case $empcheck in
+                $fullTime)
+                        empHours=8;;
+                $partTime)
+                        empHours=4;;
+                *)
+                        empHours=0;;
+        esac
+        echo $empHours
+}
+
+while [[ $totalempHours -lt $maxHoursinmonth && $totalworkingdays -lt $numberofworkingdays ]]
+do
+        ((totalworkingdays++))
+
+        empcheck=$((RANDOM%3));
+
+        totalempHours=$(($totalempHours+$empHours));
+done
+
+totalsalary=$(($totalempHours+$empRatePerHour));
